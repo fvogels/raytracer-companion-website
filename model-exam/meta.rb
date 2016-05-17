9387b268da422ec91310d7ea5d3dc9615b55e3d4
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-17 22:35:58 +0200

    model-exam

--- model-exam/meta.rb
+++ model-exam/meta.rb
@@ -3,6 +3,7 @@ require 'PovRay'
 require 'LaTeX'
 require 'RayTracer'
 require 'Upload'
+require 'Image'
 
 
 meta_object do
@@ -10,6 +11,7 @@ meta_object do
   extend LaTeX::Actions
   extend PovRay::Actions
   extend RayTracer::Actions
+  extend Image::Actions
   extend Upload::Mixin
 
   basenames = ['model-exam']
@@ -19,7 +21,21 @@ meta_object do
   tex_actions(*tex_files, group_name: :tex)
   pov_actions(*Dir['*.pov'], group_name: :pov)
   raytracer_actions(*Dir['*.cfg'], group_name: :cfg)
-  group_action(:all, [ :pov, :cfg, :tex ])
+
+  actions = Dir['*.cfg'].map do |cfg_file|
+    cfg_path = Pathname.new cfg_file
+    ppm_path = cfg_path.sub_ext '.ppm'
+    png_path = cfg_path.sub_ext '.png'
+    action_name = png_path.basename.to_s
+    
+    image_conversion(ppm_path, png_path, action_name: action_name)
+
+    action_name
+  end
+
+  group_action(:png, actions)
+      
+  group_action(:all, [ :pov, :cfg, :png, :tex ])
 
   def remote_directory
     world.parent.remote_directory + 'model-exam'
