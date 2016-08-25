e7eb1cdbedb35c446953c2f52bc59775cc7b9fcd
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-25 22:19:59 +0200

    html/features/primitives/sphere/meta.rb

--- html/features/primitives/sphere/meta.rb
+++ html/features/primitives/sphere/meta.rb
@@ -5,36 +5,11 @@ require 'Upload2'
 require 'Shortcuts'
 require 'LaTeX2'
 require 'Image2'
+require 'RayTracer3'
 require 'Contracts'
-require 'Environment'
 repo_require 'html/shared.rb'
 
 
-class Context < SharedContext
-
-end
-
-
 meta_object do
-  extend MetaData2
-  extend Template2::Actions
-  extend Upload2::Actions
-  extend Shortcuts::Actions
-
-  inherit_remote_directory(Pathname.pwd.basename.to_s)
-
-  template_files = Dir['*.template']
-  if template_files.size != 1 then
-    abort "Exactly one .template file expected"
-  else
-    template_file = template_files[0]
-  end
-  
-  bind( { :html => template(input: template_file,
-                            context: Context.new) } )
-
-  uploadable( *Dir['*.html'] )
-  uploadable( *Dir['*.png'] )
-
-  quick_all(:html)
+  instance_eval(&shared_metaobject)
 end
