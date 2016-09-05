21cde7875e29ea089bed9fa8acbaa9d40ad758f0
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-05 15:20:33 +0200

    html/design

--- html/design/meta.rb
+++ html/design/meta.rb
@@ -7,34 +7,9 @@ require 'LaTeX2'
 require 'Image2'
 require 'RayTracer3'
 require 'Contracts'
-require '../shared.rb'
-
-
-class Context < SharedContext
-
-end
+repo_require 'html/shared.rb'
 
 
 meta_object do
-  extend MetaData2
-  extend Template2::Actions
-  extend Upload2::Actions
-  extend Shortcuts::Actions
-
-  inherit_remote_directory(Pathname.pwd.basename.to_s)
-
-  template_files = Dir['*.template']
-  if template_files.size != 1 then
-    abort "Exactly one .template file expected"
-  else
-    template_file = template_files[0]
-  end
-  
-  bind( { :html => template(input: template_file,
-                            context: Context.new) } )
-
-  uploadable( *Dir['*.html'] )
-  uploadable( *Dir['*.png'] )
-
-  quick_all(:html)
+  instance_eval(&shared_metaobject)
 end
