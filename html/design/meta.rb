b1dda7a3fc3dd91ff4f29b558fea149ae0e62bdd
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-14 17:27:47 +0200

    html/design
    Removed redundant files

--- html/design/meta.rb
+++ html/design/meta.rb
@@ -1,15 +1,15 @@
 require 'MetaData2'
-require 'Html2'
-require 'Template2'
 require 'Upload2'
 require 'Shortcuts'
-require 'LaTeX2'
-require 'Image2'
-require 'RayTracer3'
-require 'Contracts'
-repo_require 'html/shared.rb'
 
 
 meta_object do
-  instance_eval(&shared_metaobject)
+  extend MetaData2
+  extend Upload2::Actions
+  extend Shortcuts::Actions
+  
+
+  inherit_remote_directory 'design'
+
+  quick_recursive_all
 end
