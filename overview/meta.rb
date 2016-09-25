f9633e354ace1a95576ccf1f86a0a676673179dd
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-25 09:34:59 +0200

    overview

---
+++ overview/meta.rb
@@ -0,0 +1,19 @@
+require 'MetaData2'
+require 'Upload2'
+require 'Shortcuts'
+require 'LaTeX2'
+require 'Image2'
+require 'Contracts'
+
+
+meta_object do
+  extend MetaData2
+  extend LaTeX2::Actions
+  extend Upload2::Actions
+  extend Shortcuts::Actions
+
+  inherit_remote_directory 'overview'
+
+  quick_all_tex
+  quick_all(:pdf)
+end
