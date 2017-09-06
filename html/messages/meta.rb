d7d9fd58d8a8173b057f00334dff4aa23989e617
Frederic Vogels <frederic.vogels@gmail.com>
2017-09-06 18:55:03 +0200

    messages

--- html/messages/meta.rb
+++ html/messages/meta.rb
@@ -3,13 +3,31 @@ require 'Html2'
 require 'Template2'
 require 'Upload2'
 require 'Shortcuts'
-require 'LaTeX2'
-require 'Image2'
-require 'RayTracer3'
 require 'Contracts'
 repo_require 'html/shared.rb'
 
 
+
 meta_object do
-  instance_eval(&shared_metaobject)
+  extend MetaData2
+  extend LaTeX2::Actions
+  extend Upload2::Actions
+  extend Shortcuts::Actions
+  extend Template2::Actions
+  
+
+  inherit_remote_directory
+
+  template_files = Dir['*.template']
+  if template_files.size != 1 then
+    STDERR.puts "Exactly one .template file expected in #{Dir.pwd}"
+    abort
+  else
+    template_file = template_files[0]
+  end
+
+  bind( { :html => template(input: template_file,
+                            context: SharedContext.new) } )
+
+  uploadable( *Dir['*.html'] )
 end
