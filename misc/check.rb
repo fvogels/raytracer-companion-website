9ae84278e1c15669b14ab9daebf3d48ed1316d87
Frederic Vogels <frederic.vogels@ucll.be>
2019-01-19 16:23:22 +0100

    misc

--- misc/check.rb
+++ misc/check.rb
@@ -1,66 +1,52 @@
-$extensions = {}
-$shareable = []
-$mutually_exclusive = Hash.new { |hash, key| hash[key] = [] }
+#!/usr/bin/env ruby
+
+
+# Students:
+#  This file does not contain much of value for you
+#  Open team-data.rb instead
+
+
+
+$extensions = []
 $team = {}
 
-def extension(id, weight, shareable: false)
-  raise 'Multiply defined extension' if $extensions.has_key? id
+MINIMAL_REQUIREMENTS_BY_DIFFICULTY = [5, 4, 3, 1, 1]
 
-  $extensions[id] = weight
-  $shareable << id if shareable
+
+Extension = Struct.new(:name, :difficulty, :allow_multiple, :mutually_exclusive) do
+  def to_s
+    name.to_s
+  end
 end
 
-def mutually_exclusive(*ids)
-  ids.each do |id|
-    raise "Unknown extension #{id}" unless $extensions.has_key? id
-  end
 
-  (0...ids.size).each do |i|
-    id = ids[i]
-    other = ids[0...i] + ids[i+1..-1]
+def extension(id, difficulty, allow_multiple: false)
+  raise "Multiply defined extension #{id}" if Object.const_defined? id
 
-    raise "Inconsistent mutually exclusive for #{id}" if $mutually_exclusive.has_key? id
-    
-    $mutually_exclusive[id] = other
+  extension = Extension.new(id, difficulty, allow_multiple, [])
+
+  Object.const_set(id, extension)
+  $extensions << extension
+end
+
+def mutually_exclusive(*extensions)
+  extensions.each do |x|
+    extensions.each do |y|
+      x.mutually_exclusive << y
+    end
   end
 end
 
 class TeamMemberContext
   def initialize(member_id)
-    raise "Team member #{member_id} already exists" if $team.has_key? member_id
-    
+    raise "#{member_id} already exists" if $team.has_key? member_id
+
     @member_id = member_id
     $team[member_id] = []
   end
-  
-  def extension(extension_id)
-    abort "Unknown extension #{extension_id} for team member #{@member_id}; use custom_extension for non-standard extensions" unless $extensions.has_key? extension_id
 
-    abort "Team member #{@member_id} registered extension #{extension_id} twice" if $team[@member_id].member? extension_id
-    
-    if (mutually_exclusive_with = find_mutually_exclusive extension_id)
-      abort "Team member #{@member_id} has mutually exclusive extensions: #{extension_id} and #{mutually_exclusive_with}"
-    end
-
-    if !$shareable.member? extension_id and (other_member = $team.keys.find do |member_id|
-          $team[member_id].member? extension_id
-        end)
-      abort "Team members #{@member_id} and #{other_member} both made extension #{extension_id}"
-    end
-
-    $team[@member_id] << extension_id
-  end
-
-  def custom_extension(extension_id)
-    abort "Custom extension's name clashes with regular extension: #{extension_id}" if $extensions.has_key? extension_id
-
-    $team[@member_id] << extension_id
-  end
-
-  def find_mutually_exclusive(extension_id)
-    $mutually_exclusive[extension_id].find do |exclusive_with|
-      $team[@member_id].member? exclusive_with
-    end
+  def extension(extension)
+    $team[@member_id] << extension
   end
 end
 
@@ -68,200 +54,246 @@ def team_member(member_id, &block)
   TeamMemberContext.new(member_id).instance_eval(&block)
 end
 
-# def remove_mutually_exclusives(extensions)
-#   result = []
 
-#   extensions.each do |extension|
-#     mutually_exclusive_with = $mutually_exclusive[extension]
+def check_mutually_exclusive
+  $team.each do |id, extensions|
+    (0...extensions.size).each do |i|
+      (i+1...extensions.size).each do |j|
+        x = extensions[i]
+        y = extensions[j]
 
-#     unless mutually_exclusive_with.any? { |x| result.member? x }
-#       result << extension
-#     end
-#   end
+        if x.mutually_exclusive.include? y
+          puts "Uh oh! #{id} has made 2 mutually exclusive extensions, namely #{x.name} and #{y.name}"
+        end
+      end
+    end
+  end
+end
 
-#   extension
-# end
+def check_duplicates
+  $team.each do |id, extensions|
+    (0...extensions.size).each do |i|
+      (i+1...extensions.size).each do |j|
+        x = extensions[i]
+        y = extensions[j]
 
-def extension_difficulties(member_id)
-  extensions = $team[member_id]
-  
-  extension_difficulties = extensions.map do |extension|
-    if $extensions.has_key? extension
-    then $extensions[extension]
-    else :custom
+        if x == y and not x.allow_multiple
+          puts "Oh oh! #{id} made #{x.name} multiple times!"
+        end
+      end
     end
   end
 
-  n0, n1, n2, n3, n4, n5 = [:custom, 1, 2, 3, 4, 5].map do |difficulty|
+  pairs = $team.to_a
+
+  (0...pairs.size).each do |i|
+    (i+1...pairs.size).each do |j|
+      id1, ext1 = pairs[i]
+      id2, ext2 = pairs[j]
+
+      (0...ext1.size).each do |k|
+        (0...ext2.size).each do |n|
+          e1 = ext1[k]
+          e2 = ext2[n]
+
+          if e1 == e2 and not e1.allow_multiple
+            puts "Uh oh! #{id1} and #{id2} have made the same extension #{e1}"
+          end
+        end
+      end
+    end
+  end
+end
+
+def check_difficulties
+  $team.each do |id, extensions|
+    counts = count_extensions_by_difficulty(extensions)
+
+    (0...counts.size).to_a.reverse.each do |i|
+      minimum = MINIMAL_REQUIREMENTS_BY_DIFFICULTY[i]
+
+      if counts[i] < minimum
+        puts "Uh oh! #{id} has insufficient extension of difficulty #{i + 1}!"
+      else
+        counts[i - 1] += counts[i] - minimum
+      end
+    end
+  end
+end
+
+def count_extensions_by_difficulty(extensions)
+  extension_difficulties = extensions.map(&:difficulty)
+
+  (1..5).map do |difficulty|
     extension_difficulties.count(difficulty)
   end
+end
 
-  { 'custom' => n0,
-    'green' => n1,
-    'green-orange' => n2,
-    'orange' => n3,
-    'orange-red' => n4 + n5 }
+def check_mesh
+  $team.each do |id, extensions|
+    if not extensions.include? Mesh
+
+      puts "Uh oh! #{id} forgot to make the Mesh extension"
+    end
+  end
 end
 
 def print_statistics
-  $team.keys.each do |member_id|
-    implemented = extension_difficulties member_id
+  $team.each do |id, extensions|
+    counts = count_extensions_by_difficulty(extensions)
 
-    puts (caption = "Team member #{member_id}")
-    puts( "-" * caption.size )
-    implemented.each do |difficulty, count|
-      puts "#{difficulty}: #{count}"
-    end
-
-    n1 = implemented['green']
-    n2 = implemented['green-orange']
-    n3 = implemented['orange']
-    n4 = implemented['orange-red']
-
-    if n4 >= 2
-      n3 += n4 - 2
-    else
-      puts "*** Insufficient orange-red extensions: #{n4} out of 2"
-    end
-
-    if n3 >= 3
-      n2 += n3 - 3
-    else
-      puts "*** Insufficient orange extensions: #{n3} out of 3"
-    end
-
-    if n2 >= 4
-      n1 += n2 - 4
-    else
-      puts "*** Insufficient green-orange extensions: #{n2} out of 4"
-    end
-
-    unless n1 >= 5
-      puts "*** Insufficient green extensions: #{n1} out of 5"
-    end
-
-    puts
+    puts "#{id} #{counts.inspect}"
   end
 end
 
-extension 'basic sample', 1, shareable: true
-extension 'basic scripting', 1, shareable: true
-extension 'ray tracer v1', 1, shareable: true
-extension 'ray tracer v2', 2
-extension 'ray tracer v3', 2
-extension 'ray tracer v4', 2
-extension 'ray tracer v5', 3
-extension 'ray tracer v6', 4
-extension 'random sampler', 2
-extension 'stratified sampler', 2
-extension 'jittered sampler', 2
-extension 'half jittered sampler', 2
-extension 'nrooks sampler', 3
-extension 'multijittered sampler', 4
-extension 'depth of field camera', 3
-extension 'fisheye camera', 4
-extension 'orthographic camera', 4
-extension 'directional light', 2
-extension 'spot light', 3
-extension 'area light', 2
-extension 'voronoi 2d', 5
-extension 'voronoi 3d', 5
-extension 'material dalmatian 2d', 5
-extension 'material vertical lines', 2
-extension 'material grid2d', 2
-extension 'material checkered 2d', 2
-extension 'material dalmatian 3d', 5
-extension 'material grid3d', 2
-extension 'material checkered 3d', 2
-extension 'material worley 2d', 3
-extension 'material worley 3d', 3
-extension 'material perlin 2d', 5
-extension 'material perlin 3d', 5
-extension 'material marble 2d', 3
-extension 'material marble 3d', 3
-extension 'material transformer 2d', 3
-extension 'material transformer 3d', 3
-extension 'material scale 2d', 3
-extension 'material scale 3d', 3
-extension 'material translate 2d', 3
-extension 'material translate 3d', 3
-extension 'material rotate 2d', 3
-extension 'material rotate 3d', 3
-extension 'plane xz', 1
-extension 'plane yz', 1
-extension 'plane xz optimized', 1
-extension 'plane yz optimized', 1
-extension 'cone along x', 3
-extension 'cone along y', 3
-extension 'cone along z', 3
-extension 'cone along x optimized', 2
-extension 'cone along y optimized', 2
-extension 'cone along z optimized', 2
-extension 'square xy', 1
-extension 'square xz', 1
-extension 'square yz', 1
-extension 'square xy optimized', 1
-extension 'square xz optimized', 1
-extension 'square yz optimized', 1
-extension 'cube', 2
-extension 'cylinder along x', 2
-extension 'cylinder along y', 2
-extension 'cylinder along z', 2
-extension 'cylinder along x optimized', 1
-extension 'cylinder along y optimized', 1
-extension 'cylinder along z optimized', 1
-extension 'triangle', 2
-extension 'triangle optimized', 1
-extension 'bounding box', 2
-extension 'mesh', 5, shareable: true
-extension 'bbh algorithm', 5
-extension 'cropper', 3
-extension 'cropper optimized', 2
-extension 'bumpifier', 5
-extension 'intersection', 5
-extension 'intersection optimized', 2
-extension 'difference', 5
-extension 'difference optimized', 2
-extension 'primitive scaling', 1
-extension 'primitive rotation x', 1
-extension 'primitive rotation y', 1
-extension 'primitive rotation z', 1
-extension 'group', 2
-extension 'edge', 5
-extension 'cartoon', 3
-extension 'parallel scheduler', 4
-extension 'motion blur', 4
-extension 'bmp', 2
-extension 'ppm', 3
-extension 'easing library', 2
-extension 'bounce', 3
-extension 'elastic', 3
-extension 'quadratic', 4
-extension 'cubic', 4
-extension 'quintic',4 
-extension 'point animation', 2
-extension 'angle animation', 2
-extension 'quaternions', 2
-extension 'circular animation', 2
-extension 'lissajous', 4
-extension 'cyclic', 3
-extension 'slicer', 3
+extension :BasicSample, 1, allow_multiple: true
+extension :BasicScripting, 1, allow_multiple: true
 
-mutually_exclusive 'bounce', 'elastic'
-mutually_exclusive 'plane xz', 'plane yz'
-mutually_exclusive 'plane xz optimized', 'plane yz optimized'
-mutually_exclusive 'cone along x', 'cone along y', 'cone along z'
-mutually_exclusive 'cone along x optimized', 'cone along y optimized', 'cone along z optimized'
-mutually_exclusive 'square xy', 'square xz', 'square yz'
-mutually_exclusive 'square xy optimized', 'square xz optimized', 'square yz optimized'
-mutually_exclusive 'cylinder along x', 'cylinder along y', 'cylinder along z'
-mutually_exclusive 'cylinder along x optimized', 'cylinder along y optimized', 'cylinder along z optimized'
-mutually_exclusive 'primitive rotation x', 'primitive rotation y', 'primitive rotation z'
-mutually_exclusive 'material scale 2d', 'material translate 2d', 'material rotate 2d'
-mutually_exclusive 'material scale 3d', 'material translate 3d', 'material rotate 3d'
-mutually_exclusive 'quadratic', 'cubic', 'quintic'
-mutually_exclusive 'point animation', 'angle animation'
+# Ray tracers
+extension :RayTracerV1, 1, allow_multiple: true
+extension :RayTracerV2, 2
+extension :RayTracerV3, 2
+extension :RayTracerV4, 2
+extension :RayTracerV5, 3
+extension :RayTracerV6, 4
+
+# Samplers
+extension :RandomSampler, 2
+extension :StratifiedSampler, 2
+extension :JitteredSampler, 2
+extension :HalfJitteredSampler, 2
+extension :NRooksSampler, 3
+extension :MultijitteredSampler, 3
+
+# Cameras
+extension :DepthOfFieldCamera, 3
+extension :FisheyeCamera, 4
+extension :OrthographicCamera, 4
+
+# Lights
+extension :DirectionalLight, 2
+extension :SpotLight, 3
+extension :AreaLight, 2
+
+# Materials
+extension :Voronoi2D, 5
+extension :Voronoi3D, 5
+extension :MaterialDalmatian2D, 5
+extension :MaterialVerticalLines, 2
+extension :MaterialGrid2d, 2
+extension :MaterialCheckered2D, 2
+extension :MaterialDalmatian3D, 5
+extension :MaterialGrid3d, 2
+extension :MaterialCheckered3D, 2
+extension :MaterialWorley2D, 3
+extension :MaterialWorley3D, 3
+extension :MaterialPerlin2D, 5
+extension :MaterialPerlin3D, 5
+extension :MaterialMarble2D, 3
+extension :MaterialMarble3D, 3
+extension :MaterialTransformer2D, 3
+extension :MaterialTransformer3D, 3
+extension :MaterialScale2D, 3
+extension :MaterialScale3D, 3
+extension :MaterialTranslate2D, 3
+extension :MaterialTranslate3D, 3
+extension :MaterialRotate2D, 3
+extension :MaterialRotate3D, 3
+
+# Primitives
+extension :PlaneXZ, 1
+extension :PlaneYZ, 1
+extension :PlaneXZOptimized, 1
+extension :PlaneYZOptimized, 1
+extension :ConeAlongX, 3
+extension :ConeAlongY, 3
+extension :ConeAlongZ, 3
+extension :ConeAlongXOptimized, 2
+extension :ConeAlongYOptimized, 2
+extension :ConeAlongZOptimized, 2
+extension :SquareXY, 1
+extension :SquareXZ, 1
+extension :SquareYZ, 1
+extension :SquareXYOptimized, 1
+extension :SquareXZOptimized, 1
+extension :SquareYZOptimized, 1
+extension :Cube, 2
+extension :CylinderAlongX, 2
+extension :CylinderAlongY, 2
+extension :CylinderAlongZ, 2
+extension :CylinderAlongXOptimized, 1
+extension :CylinderAlongYOptimized, 1
+extension :CylinderAlongZOptimized, 1
+extension :Triangle, 2
+extension :TriangleOptimized, 1
+extension :BoundingBox, 2
+extension :Cropper, 3
+extension :CropperOptimized, 2
+extension :Bumpifier, 5
+extension :Intersection, 4
+extension :IntersectionOptimized, 2
+extension :Difference, 4
+extension :DifferenceOptimized, 2
+extension :PrimitiveScaling, 1
+extension :PrimitiveRotationX, 1
+extension :PrimitiveRotationY, 1
+extension :PrimitiveRotationZ, 1
+extension :Mesh, 5, allow_multiple: true
+
+# Renderers
+extension :Group, 2
+extension :Edge, 5
+extension :Cartoon, 3
+
+# Performance
+extension :ParallelScheduler, 3, allow_multiple: true
+
+# Pipeline
+extension :MotionBlur, 4
+extension :Bmp, 2
+extension :Ppm, 3
+extension :Studio, 2
+
+# Animation
+extension :EasingLibrary, 2
+extension :Bounce, 3
+extension :Elastic, 3
+extension :Quadratic, 3
+extension :Cubic, 3
+extension :Quintic,3
+extension :PointAnimation, 2
+extension :AngleAnimation, 2
+extension :Quaternions, 2
+extension :CircularAnimation, 2
+extension :Lissajous, 3
+extension :Cyclic, 3
+extension :Slicer, 3
+
+
+
+mutually_exclusive Bounce, Elastic
+mutually_exclusive PlaneXZ, PlaneYZ
+mutually_exclusive PlaneXZOptimized, PlaneYZOptimized
+mutually_exclusive ConeAlongX, ConeAlongY, ConeAlongZ
+mutually_exclusive ConeAlongXOptimized, ConeAlongYOptimized, ConeAlongZOptimized
+mutually_exclusive SquareXY, SquareXZ, SquareYZ
+mutually_exclusive SquareXYOptimized, SquareXZOptimized, SquareYZOptimized
+mutually_exclusive CylinderAlongX, CylinderAlongY, CylinderAlongZ
+mutually_exclusive CylinderAlongXOptimized, CylinderAlongYOptimized, CylinderAlongZOptimized
+mutually_exclusive PrimitiveRotationX, PrimitiveRotationY, PrimitiveRotationZ
+mutually_exclusive MaterialScale2D, MaterialTranslate2D, MaterialRotate2D
+mutually_exclusive MaterialScale3D, MaterialTranslate3D, MaterialRotate3D
+mutually_exclusive Quadratic, Cubic, Quintic
+mutually_exclusive PointAnimation, AngleAnimation
 
 load 'team-data.rb'
+
+puts "Version 3.1"
+puts "*** Warning: this script might still change, but only in minor ways"
+
+check_mutually_exclusive
+check_duplicates
+check_difficulties
+check_mesh
 
 print_statistics
