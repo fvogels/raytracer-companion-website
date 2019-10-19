85540454c42320e6877b23cebd1ecf7581b57264
Frederic Vogels <frederic.vogels@ucll.be>
2019-10-19 15:08:19 +0200

    arnold braine fixes animations

--- html/reference/design/animations/basics/generalized-animation.cpp
+++ html/reference/design/animations/basics/generalized-animation.cpp
@@ -1,7 +1,7 @@
 Animation<double> double_animation(double from, double to, Duration duration)
 {
   std::function<double(TimeStamp)> lambda = [from, to, duration](TimeStamp now) {
-    returh from + (to - from) * now.seconds() / duration.seconds();
+    return from + (to - from) * now.seconds() / duration.seconds();
   };
 
   return make_animation( from_lambda(lambda), duration );
