bdae5e4e021ba16b3d34ec62841648f1c839e109
Frederic Vogels <frederic.vogels@ucll.be>
2022-10-28 15:42:14 +0200

    code cleanup

--- scripts/preview-block-macro.rb
+++ scripts/preview-block-macro.rb
@@ -33,7 +33,7 @@ class PreviewBlockMacro < Asciidoctor::Extensions::BlockMacroProcessor
 
       filename = document_directory.join("#{target}.chai")
       file_contents = filename.readlines.map(&:rstrip)[1..-1]
-      open_block << create_listing_block(open_block, file_contents, { 'language' => 'chai'}).tap do |listing_block|
+      open_block << create_listing_block(open_block, file_contents, { 'language' => 'chai' }).tap do |listing_block|
         listing_block.style = 'source'
         listing_block.set_option('nowrap')
         listing_block.commit_subs
